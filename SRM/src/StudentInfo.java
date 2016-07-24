8f400791dcb7441873be2a48106e86a7da8ac929
ivan.ruiz <ivan.ruiz@B107-16.soit.local>
2016-07-25 10:49:47 +1200

    Created email object in StudentInfo and added to StudentMain

--- SRM/src/StudentInfo.java
+++ SRM/src/StudentInfo.java
@@ -3,18 +3,22 @@ public class StudentInfo {
 	private String studName;
 	private int studID;
 	private String studPhone;
+	private String email;
 	
-	public StudentInfo(String studName, int studID, String studPhone) {
+	public StudentInfo(String studName, int studID, String studPhone, String email) {
 		super();
 		this.studName = studName;
 		this.studID = studID;
 		this.studPhone = studPhone;
+		this.email = email;
 	}
 	
+
 	public void displayInfo(){
 		System.out.println("Student Name: "+studName);
 		System.out.println("Student ID: "+studID);
 		System.out.println("Student Contact: "+studPhone);
+		System.out.println("Student Email: "+email);
 		
 	}
 }
--- SRM/src/StudentMain.java
+++ SRM/src/StudentMain.java
@@ -2,9 +2,9 @@
 public class StudentMain {
 
 	public static void main(String[] args) {
-		StudentInfo student1 = new StudentInfo("Joe Doe", 123456, "0229876543");
+		StudentInfo student1 = new StudentInfo("Joe Doe", 123456, "0229876543", "JoeDoe@weltec.ac.nz");
 		student1.displayInfo();		
-		StudentInfo student2 = new StudentInfo("Jane Doe", 123457, "0223566543");
+		StudentInfo student2 = new StudentInfo("Jane Doe", 123457, "0223566543", "JaneDoe@weltec.ac.nz");
 		student2.displayInfo();
 	}
 
